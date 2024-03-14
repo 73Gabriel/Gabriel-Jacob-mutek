fd9b340dc1e14577314d2a892128354259b92eda
GabrielNBA24 <gabriellolclerval@gmail.com>
2024-03-14 16:44:46 -0400

    added swiper

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var swiperlp = new Swiper('.swiper-screenshots', {
+
+    direction: 'horizontal',
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+
+
+
+const swiper = new Swiper(".swiper-filters", {
+    // Optional parameters
+    direction: "horizontal",
+    slidesPerView: 4,
+    spaceBetween: 10,
+    speed: 600,
+});
+
+const swipers = new Swiper(".swiper-featured-artworks", {
+    // Optional parameters
+    direction: "horizontal",
+    slidesPerView: 2,
+    spaceBetween: -800,
+    speed: 600,
+});
