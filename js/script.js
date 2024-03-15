311d54e27a85f5d9924800e9762ae54a22f681b9
GabrielNBA24 <gabriellolclerval@gmail.com>
2024-03-15 17:25:59 -0400

    updated the website
    Téléphone mis dans avec swiper parametres

--- js/script.js
+++ js/script.js
@@ -8,23 +8,3 @@ var swiperlp = new Swiper('.swiper-screenshots', {
         prevEl: '.swiper-button-prev',
     },
 });
-
-
-
-
-
-const swiper = new Swiper(".swiper-filters", {
-    // Optional parameters
-    direction: "horizontal",
-    slidesPerView: 4,
-    spaceBetween: 10,
-    speed: 600,
-});
-
-const swipers = new Swiper(".swiper-featured-artworks", {
-    // Optional parameters
-    direction: "horizontal",
-    slidesPerView: 2,
-    spaceBetween: -800,
-    speed: 600,
-});
